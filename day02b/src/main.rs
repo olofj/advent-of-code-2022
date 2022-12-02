f95fc759f26fb183be2b1cd7856d1f228f329d4a
Olof Johansson <olof@lixom.net>
2022-12-02 10:19:36 -0800

    day02b: working
    Signed-off-by: Olof Johansson <olof@lixom.net>

---
+++ day02b/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let sum: usize = include_str!("input.txt")
+        .lines()
+        .map(|l| { 
+            let b = l.as_bytes(); 
+            (b[0] - b'A', b[2] - b'X')
+        })
+        .map(|(a,b)| 
+            match b {
+                0 => (3+a-1)%3,
+                1 => a + 3,
+                2 => (a+1)%3  + 6,
+                _ => panic!("invalid input")
+            } as usize + 1
+        )
+        .sum();
+
+    println!("sum {:?}", sum);
+}
+
+// 14859
