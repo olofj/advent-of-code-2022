fc65ea59fc4fa44330ecc81f25c0508d76543276
Olof Johansson <olof@lixom.net>
2022-12-02 10:13:54 -0800

    day02a: short and clean
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- day02a/src/main.rs
+++ day02a/src/main.rs
@@ -1,26 +1,18 @@
 fn main() {
-    let input: Vec<(u8, u8)> = include_str!("input.txt")
+    let sum: usize = include_str!("input.txt")
         .lines()
-        .map(|l| { 
-            let b = l.as_bytes(); 
+        .map(|l| {
+            let b = l.as_bytes();
             (b[0] - b'A', b[2] - b'X')
         } )
-        .collect();
-
-    let base:usize = input.iter()
-        .map(|(_,b)| *b as usize + 1 )
-        .sum();
-
-    let outcomes: usize = input.iter()
-        .map(|(a,b)| { 
+        .map(|(a,b)|
             match (3+b-a)%3 {
                 1 => 6,
                 0 => 3,
                 _ => 0,
-            }
-        })
+            } + b as usize + 1
+        )
         .sum();
 
-    println!("base {:?}", base);
-    println!("sum {:?}", base+outcomes);
+    println!("sum {:?}", sum);
 }
