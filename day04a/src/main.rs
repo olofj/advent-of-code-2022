66f20b0c9d96b3741c82b541d19ec42d10c68645
Olof Johansson <olof@lixom.net>
2022-12-03 22:25:37 -0800

    day04a: working
    Signed-off-by: Olof Johansson <olof@lixom.net>

---
+++ day04a/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let input = include_str!("input.txt")
+        .lines();
+    
+    let mut count:usize = 0;
+
+    for l in input {
+        let (p1, p2) = l.split_once(",").unwrap();
+        let (s1,e1) = p1.split_once("-").unwrap();
+        let (s2,e2) = p2.split_once("-").unwrap();
+        let s1 = s1.parse::<usize>().unwrap();
+        let e1 = e1.parse::<usize>().unwrap();
+        let s2 = s2.parse::<usize>().unwrap();
+        let e2 = e2.parse::<usize>().unwrap();
+        if (s1 <= s2 && e1 >= e2) || (s2 <= s1 && e2 >= e1) {
+            count += 1;
+        }
+    }
+    println!("count {}", count);
+}
