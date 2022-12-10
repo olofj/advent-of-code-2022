a209880548b31e562e8a2ed0e82b5af6e27ef319
Olof Johansson <olof@lixom.net>
2022-12-10 15:00:42 -0800

    day08b: cleaned up
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- day08b/src/main.rs
+++ day08b/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+
 fn main() {
     let grid = include_str!("input.txt")
         .lines()
@@ -9,21 +11,21 @@ fn main() {
 
     let visible = (0..rows).map(|row| {
         (0..cols).map(|col| {
-            let mut up = (0..row).rev().map_while(|r| if grid[r][col] < grid[row][col] { Some(1) } else { None }).sum::<usize>();
-            if up < row { up += 1 } 
-            let mut down = (row+1..rows).map_while(|r| if grid[r][col] < grid[row][col] { Some(1) } else { None }).sum::<usize>();
-            if down < rows-row-1 { down += 1 }
-            let mut left = (0..col).rev().map_while(|c| if grid[row][c] < grid[row][col] { Some(1) } else { None }).sum::<usize>();
-            if left < col { left += 1 }
-            let mut right = (col+1..cols).map_while(|c| if grid[row][c] < grid[row][col] { Some(1) } else { None }).sum::<usize>();
-            if right < cols - col - 1 { right += 1 }
-            println!("{} {}: {} {} {} {}", row, col, up, down, left, right);
+            let g = grid[row][col];
+            let mut up = 1+(0..row).rev().map_while(|r| if grid[r][col] < g { Some(1) } else { None }).sum::<usize>();
+            up = min(up, row);
+            let mut down = 1+(row+1..rows).map_while(|r| if grid[r][col] < g { Some(1) } else { None }).sum::<usize>();
+            down = min(down, rows-row-1);
+            let mut left = (0..col).rev().map_while(|c| if grid[row][c] < g { Some(1) } else { None }).sum::<usize>();
+            left = min(left, col);
+            let mut right = (col+1..cols).map_while(|c| if grid[row][c] < g { Some(1) } else { None }).sum::<usize>();
+            right = min(right, cols-col-1);
             up * down * left * right
         })
         .collect::<Vec<usize>>()
     })
     .collect::<Vec<Vec<usize>>>();
-    println!("Array: {:?}", visible);
+
     let max = visible.iter().map(|r| r.iter().max().unwrap()).max().unwrap();
     println!("Max: {}", max);
 }
