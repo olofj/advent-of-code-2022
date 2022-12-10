3e388c331fec94664f145462e7fb5603749778fa
Olof Johansson <olof@lixom.net>
2022-12-10 15:08:28 -0800

    day08a: cleaned up
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- day08a/src/main.rs
+++ day08a/src/main.rs
@@ -1,27 +1,33 @@
 fn main() {
     let grid = include_str!("input.txt")
         .lines()
-        .map(|l| l.as_bytes().iter().map(|c| (c-b'0') as usize).collect::<Vec<usize>>())
+        .map(|l| {
+            l.as_bytes()
+                .iter()
+                .map(|c| (c - b'0') as usize)
+                .collect::<Vec<usize>>()
+        })
         .collect::<Vec<Vec<usize>>>();
 
     let rows = grid.len();
     let cols = grid[0].len();
 
-    let visible = (0..rows).map(|row| {
-        (0..cols).map(|col| {
-            col == 0 || row == 0 || col == cols-1 || row == rows-1 ||
-            (0..row).all(|r| grid[r][col] < grid[row][col]) ||
-            (row+1..rows).all(|r| grid[r][col] < grid[row][col]) ||
-            (0..col).all(|c| grid[row][c] < grid[row][col]) ||
-            (col+1..cols).all(|c| grid[row][c] < grid[row][col])
+    let visible = (0..rows)
+        .map(|row| {
+            (0..cols)
+                .map(|col| {
+                    let g = grid[row][col];
+                    (col == 0
+                        || row == 0
+                        || col == cols - 1
+                        || row == rows - 1
+                        || (0..row).all(|r| grid[r][col] < g)
+                        || (row + 1..rows).all(|r| grid[r][col] < g)
+                        || (0..col).all(|c| grid[row][c] < g)
+                        || (col + 1..cols).all(|c| grid[row][c] < g)) as usize
+                })
+                .sum::<usize>()
         })
-//        .map(|v| if v { 1 } else { 0 } )
-//        .sum::<usize>()
-        .collect::<Vec<bool>>()
-    })
-//    .sum();
-    .collect::<Vec<Vec<bool>>>();
-    println!("Array: {:?}", visible);
-    let count = visible.iter().map(|r| r.iter().map(|c| if *c { 1 } else { 0 } ).sum::<usize>()).sum::<usize>();
-    println!("Count: {:?}", count);
+        .sum::<usize>();
+    println!("visible: {:?}", visible);
 }
