93e3c2b2f73670eaa2d5dc9744a7291974136401
Olof Johansson <olof@lixom.net>
2022-12-10 17:39:31 -0800

    day07a: cleaned up
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- day07a/src/main.rs
+++ day07a/src/main.rs
@@ -1,92 +1,64 @@
 use std::collections::HashMap;
-use itertools::Itertools;
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 struct Dir {
-    files: Vec<(String, usize)>,
-    dirs: Vec<String>,
+    filsz: usize,
 }
 
 fn main() {
     let mut cwd: Vec<&str> = Vec::new();
-    let mut root = Dir {
-        files: Vec::new(),
-        dirs: Vec::new(),
-    };
-
-    let input: Vec<Vec<&str>> = include_str!("input.txt")
-        .split("$")
-        .skip(1)
-        .map(|c| c.lines().collect::<Vec<&str>>())
-        .collect();
-//    println!("{:?}", input);
+    let root = Dir { filsz: 0 };
 
     let mut dirs: HashMap<String, Dir> = HashMap::new();
     dirs.insert("".to_string(), root);
 
-    for cc in input {
-        let mut c = cc.iter();
-        let cmd: Vec<&str> = c
-            .next()
-            .unwrap()
-            .split_whitespace()
-            .collect();
-        match cmd[0] {
-            "cd" => {
-//                println!("CD {:?}", cmd);
-                match cmd[1] {
+    include_str!("input.txt")
+        .split("$")
+        .skip(1)
+        .map(|c| c.lines().collect::<Vec<&str>>())
+        .for_each(|cc| {
+            let mut c = cc.iter();
+            let cmd: Vec<&str> = c.next().unwrap().split_whitespace().collect();
+            match cmd[0] {
+                "cd" => match cmd[1] {
                     ".." => {
                         cwd.pop();
-                    },
+                    }
                     "/" => {
                         cwd = Vec::new();
-                    },
+                    }
                     _ => {
-                        let parent = cwd.join("/");
                         cwd.push(cmd[1]);
-                        dirs.get_mut(&parent).unwrap().dirs.push(cwd.join("/"));
-                        dirs.insert(cwd.join("/"), Dir {
-                            files: Vec::new(),
-                            dirs: Vec::new(),
-                        });
-                    },
+                    }
+                },
+                "ls" => {
+                    let fsz = c
+                        .filter_map(|l| l.split_whitespace().nth(0).unwrap().parse::<usize>().ok())
+                        .sum();
+                    dirs.insert(cwd.join("/"), Dir { filsz: fsz });
                 }
-//                println!("CWD {:?}", cwd.join("/"));
-            },
-            "ls" => {
-//                println!("LS {:?}", cmd);
-                let files: HashMap<String, usize> = c
-                    .map(|e| e
-                        .split_whitespace()
-                        .collect::<Vec<&str>>()
-                    )
-                    .filter(|l| !l[0].eq("dir"))
-                    .map(|l| (l[0].to_string(), l[0].parse::<usize>().unwrap()))
-                .collect();
-                dirs.get_mut(&cwd.join("/")).unwrap().files.extend(files);
-            },
-            _ => {
-                println!("Unknown command {:?}", cmd);
-            },
+                _ => {
+                    println!("Unknown command {:?}", cmd);
+                }
+            }
+        });
+
+    let dsz = {
+        |d: &String| {
+            dirs.iter()
+                .filter_map(|(n, dir)| {
+                    if n.starts_with(d.as_str()) {
+                        Some(dir.filsz)
+                    } else {
+                        None
+                    }
+                })
+                .sum()
         }
-    }
+    };
+    let dirsizes: Vec<(&String, usize)> = dirs.keys().map(|d| (d, dsz(d))).collect();
+    let total: usize = dirsizes.iter().filter(|(d,sz)| *sz < 100000).map(|(_, sz)| sz).sum();
 
-    let alldirs = dirs.keys().collect::<Vec<&String>>();
+    println!("total: {}", total);
 
-    let dirsz: Vec<(&String, usize)> = alldirs.iter().map(|d| (*d, dirs.keys()
-        .filter(|n| n.starts_with(d.as_str()))
-        .map(|sd| dirs
-             .get(sd)
-             .unwrap()
-             .files
-             .iter()
-             .map(|(_, fsize)| fsize)
-             .sum::<usize>()
-        )
-        .sum()
-    ))
-    .collect();
-
-    let total: usize = dirsz.iter().filter(|(d,sz)| *sz < 100000).map(|(_, sz)| sz).sum();
-    println!("total {}", total);
 }
