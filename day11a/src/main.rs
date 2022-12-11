ece6dd3c7b8f8d4476cf24d3f8a657442e17bdab
Olof Johansson <olof@lixom.net>
2022-12-11 11:19:54 -0800

    day11a: some cleanup
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- day11a/src/main.rs
+++ day11a/src/main.rs
@@ -1,69 +1,101 @@
-struct Monkey<'a> {
+type Update = fn(usize, Option<usize>) -> usize;
+
+struct Monkey {
     items: Vec<usize>,
-    op: &'a str,
+    update: Update,
     arg: Option<usize>,
     divisible: usize,
-    if_true: usize,
-    if_false: usize,
+    next: [usize; 2],
 }
+
 fn main() {
     let input = include_str!("input.txt")
         .split("\n\n")
         .collect::<Vec<&str>>();
-    
-    let mut monkey: Vec<Monkey> = Vec::new();
 
-    for (i,m) in input.iter().enumerate() {
-        let m = m.lines().collect::<Vec<&str>>();
-        let items: Vec<_> = m[1].split_once(":").unwrap().1.split(&[',',' '][..]).filter_map(|i| i.parse::<usize>().ok()).collect::<Vec<_>>();
-        println!("items: {:?}", items);
-        let div = m[3].split_once(" by ").unwrap().1.parse::<usize>().unwrap();
-        println!("div: {}", div);
-        let op = m[2].split_once("new = old ").unwrap().1.split_whitespace().collect::<Vec<&str>>();
-        let arg = op[1].parse::<usize>().ok();
-        let op = match op[0] {
-            "+" => "+",
-            "*" => "*",
-            _ => panic!("Unkown operator {}", op[0]),
+    let mut monkey = input.iter().map(|m| m.lines().collect::<Vec<&str>>()).map(|l| {
+        let mut new = Monkey {
+            items: Vec::new(),
+            update: |_, _| -> usize { 0 },
+            arg: None,
+            divisible: 0,
+            next: [0, 0],
         };
-        println!("op {:?} arg {:?}", op, arg);
-        let if_true = m[4].split_once("throw to monkey ").unwrap().1.parse::<usize>().unwrap();
-        let if_false = m[5].split_once("throw to monkey ").unwrap().1.parse::<usize>().unwrap();
-        println!("if_true {} if_false {}", if_true, if_false);
-        monkey.insert(i, Monkey{ items: items, op: op, arg: arg, divisible: div, if_true: if_true, if_false: if_false});
-    }
+        l.iter().enumerate().for_each(|(i, l)| match i {
+            1 => {
+                new.items = l
+                    .split_once(": ")
+                    .unwrap()
+                    .1
+                    .split(", ")
+                    .filter_map(|i| i.parse::<usize>().ok())
+                    .collect::<Vec<_>>();
+            }
+            2 => {
+                let op = l
+                    .split_once("new = old ")
+                    .unwrap()
+                    .1
+                    .split_whitespace()
+                    .collect::<Vec<&str>>();
+                new.arg = op[1].parse::<usize>().ok();
+                new.update = match (op[0], new.arg) {
+                    ("+", None) => |old, _| -> usize { old + old },
+                    ("+", Some(_)) => |old, arg| -> usize { old + arg.unwrap() },
+                    ("*", None) => |old, _| -> usize { old * old },
+                    ("*", Some(_)) => |old, arg| -> usize { old * arg.unwrap() },
+                    _ => panic!("Unkown operator {}", op[0]),
+                };
+            }
+            3 => {
+                new.divisible = l.split_once(" by ").unwrap().1.parse::<usize>().unwrap();
+            }
+            4 => {
+                new.next[1] = l
+                    .split_once("throw to monkey ")
+                    .unwrap()
+                    .1
+                    .parse::<usize>()
+                    .unwrap();
+            }
+            5 => {
+                new.next[0] = l
+                    .split_once("throw to monkey ")
+                    .unwrap()
+                    .1
+                    .parse::<usize>()
+                    .unwrap();
+            }
+            _ => {}
+        });
 
-    let mut insp: Vec<usize> = monkey.iter().map(|_| 0).collect::<Vec<usize>>();
+        new
+    })
+    .collect::<Vec<Monkey>>();
 
-    for i in 0..20 {
+    let mut insp = vec![0; monkey.len()];
+
+    for _ in 0..20 {
         for m in 0..monkey.len() {
-            let moves = monkey[m].items.iter().map(|item| {
-                let arg = match &monkey[m].arg {
-                    None => item,
-                    Some(x) => x,
-                };
-                let new = match monkey[m].op {
-                    "*" => item * arg,
-                    "+" => item + arg,
-                    _ => panic!("Unknown op {}", monkey[m].op),
-                } / 3;
-                let check: bool = new % monkey[m].divisible == 0;
-                let next = match check {
-                    true => monkey[m].if_true,
-                    false => monkey[m].if_false,
-                };
-                (next, new)
-            }).collect::<Vec<(usize, usize)>>();
+            let moves = monkey[m]
+                .items
+                .iter()
+                .map(|item| {
+                    let new = (monkey[m].update)(*item, monkey[m].arg) / 3;
+                    let check: bool = new % monkey[m].divisible == 0;
+                    let next = monkey[m].next[check as usize];
+                    (next, new)
+                })
+                .collect::<Vec<(usize, usize)>>();
             monkey[m].items = Vec::new();
             insp[m] += moves.len();
-            moves.iter().for_each(|(next, new)| monkey[*next].items.push(*new));
-        }
-        for m in 0..monkey.len() {
-            println!("Monkey {} items: {:?}", m, monkey[m].items);
+            moves
+                .iter()
+                .for_each(|(next, new)| monkey[*next].items.push(*new));
         }
     }
     insp.sort();
-    let insp = insp.into_iter().rev().collect::<Vec<usize>>();
+    let insp = insp.into_iter().rev().take(2).collect::<Vec<usize>>();
     println!("Inspections {:?}", insp);
     println!("Monkey Business: {}", insp[0] * insp[1]);
 }
