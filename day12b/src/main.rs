98b18f1b775b79b8d75a251d65363974066feaff
Olof Johansson <olof@lixom.net>
2022-12-11 23:23:47 -0800

    day12b: show paths
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- day12b/src/main.rs
+++ day12b/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-const MOVES: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+const MOVES: [(isize, isize, char); 4] = [(-1, 0, 'v'), (1, 0, '^'), (0, -1, '>'), (0, 1, '<')];
 
 fn main() {
     let mut end: (usize, usize) = (0, 0);
@@ -41,7 +41,7 @@ fn main() {
         .iter()
         .map(|&start| {
             let mut moves: VecDeque<(usize, usize)> = VecDeque::new();
-            let mut steps: Vec<Vec<usize>> = vec![vec![0; ymax as usize]; xmax as usize];
+            let mut steps: Vec<Vec<isize>> = vec![vec![-1; ymax as usize]; xmax as usize];
 
             steps[start.0][start.1] = 0;
 
@@ -54,16 +54,42 @@ fn main() {
                     let my = (y as isize + m.1) as usize;
                     let Some(&gridlevel) = input.get(mx).and_then(|l| l.get(my)) else { continue };
                     let Some(&gridsteps) = steps.get(mx).and_then(|l| l.get(my)) else { continue };
-                    if gridlevel <= curlevel + 1 && (gridsteps > cur + 1 || gridsteps == 0) {
+                    if gridlevel <= curlevel + 1 && (gridsteps > cur + 1 || gridsteps == -1) {
                         steps[mx][my] = cur + 1;
                         moves.push_back((mx, my));
                     }
                 }
             }
+
+            if steps[end.0][end.1] != -1 {
+                let mut path: Vec<Vec<char>> = vec![vec!['.'; ymax as usize]; xmax as usize];
+                path[end.0][end.1] = 'E';
+                path[start.0][start.1] = 'S';
+
+                let mut cur = end;
+                while steps[cur.0][cur.1] != 0 {
+                    for m in MOVES {
+                        let mx = (cur.0 as isize + m.0) as usize;
+                        let my = (cur.1 as isize + m.1) as usize;
+                        let Some(&s) = steps.get(mx).and_then(|l| l.get(my)) else { continue };
+                        if s == steps[cur.0][cur.1] - 1 {
+                            path[mx][my] = m.2;
+                            cur = (mx, my);
+                            break;
+                        }
+                    }
+                }
+                print!("\x1B[2J");
+                println!("Moves for the shorterst path:");
+                for l in path {
+                    println!("{}", l.iter().collect::<String>());
+                }
+            }
+
             steps[end.0][end.1]
         })
         .filter(|x| *x > 0)
-        .collect::<Vec<usize>>();
+        .collect::<Vec<isize>>();
 
     println!("allstarts {:?}", allstarts);
     println!("min: {}", allstarts.iter().min().unwrap());
