6c4feab9ded9f05cef94ad2bc79673c73cebc05e
Olof Johansson <olof@lixom.net>
2022-12-15 13:14:05 -0800

    day15b: working and reasonably fast
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- day15b/src/main.rs
+++ day15b/src/main.rs
@@ -1,8 +1,8 @@
-use std::collections::HashSet;
-
 fn distance((sx, sy): (isize, isize), (bx, by): (isize, isize)) -> isize {
     (sx.abs_diff(bx) + sy.abs_diff(by)) as isize
 }
+
+const SZ: isize = 4000000;
 
 fn main() {
     let input = include_str!("input.txt")
@@ -24,18 +24,13 @@ fn main() {
         .map(|(s, b)| (s, distance(s, b)))
         .collect::<Vec<((isize, isize), isize)>>();
 
-    println!("input: {:?}", input);
-    //    Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-    //
-    let mut ranges: Vec<HashSet<isize>> = Vec::new();
+    let mut ranges: Vec<(isize, Vec<(isize, isize)>)> = Vec::new();
 
-    for y in 0..4000000 {
-        println!("{}", y);
-        let newrange = input
+    for y in 0..SZ {
+        let mut newrange = input
             .iter()
-            .map(|((sx, sy), d)| {
+            .filter_map(|((sx, sy), d)| {
                 let dist = sy.abs_diff(y) as isize;
-                let mut hs: Vec<isize> = Vec::new();
                 if dist <= *d {
                     /*
                     println!(
@@ -46,19 +41,60 @@ fn main() {
                         dist
                     );
                     */
-                    hs = (sx - (d - dist)..=sx + (d - dist)).collect::<Vec<isize>>();
+                    let start: isize = 0.max(sx - (d - dist));
+                    let end: isize = SZ.min(sx + (d - dist));
+                    Some((start, end))
+                } else {
+                    None
                 }
-                hs
             })
-            .flatten()
-            .collect::<HashSet<isize>>();
-//        println!("count: {}", newrange.len());
-        ranges.insert(y as usize, newrange);
+            .collect::<Vec<(isize, isize)>>();
+
+        newrange.sort_by(|a, b| a.0.cmp(&b.0));
+
+        // println!("range: {:?}", newrange);
+
+        let pruned = newrange
+            .iter()
+            .fold(Vec::<(isize, isize)>::new(), |mut acc, (s, e)| {
+                if let Some(last) = acc.last_mut() {
+                    if last.1 >= *s - 1 {
+                        last.1 = last.1.max(*e)
+                    } else {
+                        acc.push((*s, *e))
+                    }
+                } else {
+                    acc.push((*s, *e))
+                }
+                acc
+            });
+
+        // println!("pruned: {:?}", pruned);
+        let mut inverse: Vec<(isize, isize)> = Vec::new();
+        if pruned.len() > 1 {
+            let mut last_end = -1;
+            for p in pruned {
+                if last_end + 1 < p.0 {
+                    inverse.push((last_end + 1, p.0 - 1));
+                }
+                last_end = p.1;
+            }
+            // println!("inverse: {:?}", inverse);
+            ranges.push((y, inverse));
+        }
     }
 
-    for y in 0..4000000 {
-        println!("Row {}: {:?}", y, (0..20).collect::<HashSet<isize>>().difference(&ranges[y]));
+    if ranges.len() != 1 {
+        panic!(
+            "I should only get a single location for a possible beacong. Got: {:?}",
+            ranges
+        );
     }
+    let (x, y) = (ranges[0].1[0].0, ranges[0].0);
 
-    //    println!("input: {:?}", input);
+    println!("Location: {} {}", x, y);
+
+    let tunefreq = x * 4000000 + y;
+
+    println!("tunefreq: {}", tunefreq);
 }
